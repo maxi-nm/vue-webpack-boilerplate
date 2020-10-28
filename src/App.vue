8ad21d6723982b685dc0ac1729cdce8056998731
maxi-nm <em.nietomontoya@gmail.com>
2020-10-28 11:46:21 -0300

    feat: added vue main files

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>App vue</h1>
+  </div>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import Vue from 'vue';
+import App from './App.vue';
+
+const v = new Vue({
+  el: '#app',
+  render: (h) => h(App),
+});
